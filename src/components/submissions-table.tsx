725553afa4c2d96c194af3a560c5b858df13452e
Evgenii Evstropov <evstropov@arrival.com>
2023-03-23 14:43:09 +0100

    initial setup + table

---
+++ src/components/submissions-table.tsx
@@ -0,0 +1,23 @@
+import {Data} from "../data/type";
+
+const columns = [
+    {
+        title: 'Team name',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: 'Age',
+        dataIndex: 'age',
+        key: 'age',
+    },
+    {
+        title: 'Address',
+        dataIndex: 'address',
+        key: 'address',
+    },
+];
+
+export const Table = (data: Data) => {
+    return
+}
