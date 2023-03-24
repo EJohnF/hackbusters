fab011a9b6a4ac82b7a9f5b3ed9535e327189abc
Vladislav Levitskii <dev.vladlevitsky@gmail.com>
2023-03-24 11:28:05 +0100

    fix: upd server link

--- src/data/server.ts
+++ src/data/server.ts
@@ -1,4 +1,4 @@
-export const serverLink = 'https://whole-places-start-34-142-176-15.loca.lt';
+export const serverLink = 'https://honest-years-kiss-34-142-176-15.loca.lt';
 
 export const headers = {
     "Bypass-Tunnel-Reminder": 'true'
